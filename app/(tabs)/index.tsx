0fcd87e890cbf0f46a7c0f6406e3dee8fc7b29ba
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2023-07-24 13:47:50 -0400

    play animation on refresh

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,32 @@
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native"
+import { Platform, RefreshControl, SafeAreaView, ScrollView, StyleSheet } from "react-native"
 
 import EditScreenInfo from "../../components/EditScreenInfo"
 import { Text, View } from "../../components/Themed"
 import Lottie from "lottie-react-native"
+import { useRef } from "react"
 
 export default function TabOneScreen() {
+  const animationRef = useRef<Lottie>(null)
+
   return (
     <SafeAreaView>
-      <ScrollView>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({ android: 30 }),
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            onRefresh={() => {
+              animationRef.current?.play()
+            }}
+            tintColor={"transparent"}
+          />
+        }
+      >
         <Lottie
+          ref={animationRef}
           source={require("../../assets/lottie-animations/threads.json")}
           loop={false}
           autoPlay

8ad63a0a480ac778005dc2e3986963ac0411916e
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2023-07-26 12:04:44 -0400

    Post left side completed

--- components/ThreadsItem.tsx
+++ components/ThreadsItem.tsx
@@ -1,4 +1,4 @@
-import { View, useColorScheme } from "react-native"
+import { StyleSheet, View, useColorScheme } from "react-native"
 import React from "react"
 import { Thread } from "../types/threads"
 import { Text } from "./Themed"
@@ -11,10 +11,10 @@ const blurhash =
 
 function ThreadsItem(thread: Thread) {
   return (
-    <View>
-      <Text>{thread.author.username}</Text>
+    <View style={styles.container}>
+      <PostLeftSide {...thread} />
 
-      <View>
+      <View style={{ flexShrink: 1, gap: 6 }}>
         <PostHeading name={thread.author.name} createdAt={thread.createdAt} verified={thread.author.verified} />
         <Text>{thread.content}</Text>
         {thread.image && (
@@ -34,6 +34,48 @@ function ThreadsItem(thread: Thread) {
 }
 
 export default ThreadsItem
+
+function PostLeftSide(thread: Thread) {
+  const currentTheme = useColorScheme()
+  const borderColor = currentTheme === "light" ? "#00000020" : "#ffffff20"
+
+  return (
+    <View style={{ justifyContent: "space-between" }}>
+      <Image source={thread.author.photo} style={styles.image} placeholder={blurhash} contentFit="cover" transition={500} />
+
+      <View
+        style={{
+          borderWidth: 1,
+          alignSelf: "center",
+          borderColor: borderColor,
+          flexGrow: 1,
+        }}
+      />
+      <View
+        style={{
+          width: 20,
+          alignItems: "center",
+          alignSelf: "center",
+          gap: 3,
+        }}
+      >
+        <View style={{ width: 20, alignItems: "center", alignSelf: "center", gap: 3 }}>
+          {[1, 2, 3].map(index => (
+            <Image
+              key={index}
+              // @ts-ignore
+              source={thread.replies[index - 1]?.author.photo}
+              style={{ width: index * 7, height: index * 7, borderRadius: 15 }}
+              placeholder={blurhash}
+              contentFit="cover"
+              transition={500}
+            />
+          ))}
+        </View>
+      </View>
+    </View>
+  )
+}
 
 function PostHeading({ name, createdAt, verified }: { name: string; createdAt: string; verified: boolean }) {
   return (
@@ -91,3 +133,16 @@ function BottomIcons() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 6,
+    paddingBottom: 30,
+  },
+  image: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+})

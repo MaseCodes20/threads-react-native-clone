5e6d53c1f2ddf1cd3092c1d6208c697f3e8c1094
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2023-07-26 11:46:12 -0400

    Completed the right side of the thread

--- components/ThreadsItem.tsx
+++ components/ThreadsItem.tsx
@@ -1,9 +1,13 @@
-import { View } from "react-native"
+import { View, useColorScheme } from "react-native"
 import React from "react"
 import { Thread } from "../types/threads"
 import { Text } from "./Themed"
-import { MaterialIcons, Feather } from "@expo/vector-icons"
+import { MaterialIcons, Feather, FontAwesome, Ionicons, AntDesign } from "@expo/vector-icons"
 import { timeAgo } from "../utils/timeAgo"
+import { Image } from "expo-image"
+
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj["
 
 function ThreadsItem(thread: Thread) {
   return (
@@ -12,6 +16,18 @@ function ThreadsItem(thread: Thread) {
 
       <View>
         <PostHeading name={thread.author.name} createdAt={thread.createdAt} verified={thread.author.verified} />
+        <Text>{thread.content}</Text>
+        {thread.image && (
+          <Image
+            source={thread.image}
+            style={{ width: "100%", minHeight: 300, borderRadius: 10 }}
+            placeholder={blurhash}
+            contentFit="cover"
+            transition={200}
+          />
+        )}
+        <BottomIcons />
+        <PostFooter replies={thread.repliesCount} likes={thread.likesCount} />
       </View>
     </View>
   )
@@ -21,7 +37,14 @@ export default ThreadsItem
 
 function PostHeading({ name, createdAt, verified }: { name: string; createdAt: string; verified: boolean }) {
   return (
-    <View>
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexGrow: 1,
+      }}
+    >
       <View
         style={{
           flexDirection: "row",
@@ -42,6 +65,29 @@ function PostHeading({ name, createdAt, verified }: { name: string; createdAt: s
         <Text style={{ color: "gray" }}>{timeAgo(createdAt)}</Text>
         <Feather name="more-horizontal" size={14} color="gray" />
       </View>
+    </View>
+  )
+}
+
+function PostFooter({ replies, likes }: { replies: number; likes: number }) {
+  return (
+    <Text style={{ color: "gray" }}>
+      {replies} replies · {likes} likes
+    </Text>
+  )
+}
+
+function BottomIcons() {
+  const iconSize = 20
+  const currentTheme = useColorScheme()
+  const iconColor = currentTheme === "dark" ? "white" : "black"
+
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+      <FontAwesome name="heart-o" size={iconSize} color={iconColor} />
+      <Ionicons name="chatbubble-outline" size={iconSize} color={iconColor} />
+      <AntDesign name="retweet" size={iconSize} color={iconColor} />
+      <Feather name="send" size={iconSize} color={iconColor} />
     </View>
   )
 }
